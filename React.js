74c2975fd1fa1bb15d1cc4137e390c8d89287e28
Dinesh Chaudhari <chaudharidinesh538@gmail.com>
2024-02-28 01:34:07 +0530

    React Components

--- React.js
+++ React.js
@@ -1,42 +1,33 @@
-        //    How to create hello world program from react
-        // const heading = React.createElement("h1", {id : "dinesh"}, "Hello World from React")
-        // console.log(heading) // object 
-        // const root = ReactDOM.createRoot(document.getElementById("root"));
-        // root.render(heading);
 
-        /*
-        <div id ="parent">
-            <div id = "child1">
-              <h1> I am H1 tag </h1>
-              <h2> I am H2 tag </h2>
-            </div>
-            <div id = "child2">
-              <h1> I am H3 tag </h1>
-              <h2> I am H4 tag </h2>
-            </div>
-        <div/>
-        */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = 
+                <div>
+                <h1 className="Dinesh"> react element using JSX </h1> 
+                <h2> New react Element </h2> 
+                </div>
 
 
-       const heading =  React.createElement(
-                           "div", 
-                           {id : "parent"}, [
-                            React.createElement(
-                                                "div", 
-                                                 {id : "child1"}, 
-                                                 [ React.createElement( "h1", {}, "I am H1 Tag"), 
-                                                   React.createElement( "h2", {}, "I am H2 Tag") ]
-                                                 ),
-                                                 React.createElement(
-                                                    "div", 
-                                                     {id : "child2"}, 
-                                                     [ React.createElement( "h1", {}, "I am H3 Tag"), 
-                                                       React.createElement( "h2", {}, "I am H4 Tag") ]
-                                                     )
-                                                 ]
-                            )
 
-         const root = ReactDOM.createRoot(document.getElementById("root"));
-         root.render(heading);
+const Functionalcomponent =() => {
+  return <h1 className="Dinesh"> react element using functional component </h1> 
+}
+
+const Functionalcomponent2 = () =>
+  
+  <div id="container">
+    {heading}                                       //*Injecting react element in Component
+  <h2> New react Element </h2> 
+ <Functionalcomponent />                            //*Three ways write react component inside another component(Component Composition)
+ <Functionalcomponent> </Functionalcomponent>
+ {Functionalcomponent()}
+ </div>
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Functionalcomponent2 />);
 
 
